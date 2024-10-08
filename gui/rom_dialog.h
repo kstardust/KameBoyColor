561e4ac9325425fa0a2a298da6e6ba4ad13a70a8
stardust <lql9705@gmail.com>
2024-10-08 18:44:16 +0900

    add: rom file dialog

---
+++ gui/rom_dialog.h
@@ -0,0 +1,14 @@
+#ifndef _ROM_DIALOG_H
+#define _ROM_DIALOG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int RomDialog(char **cartidge, char **boot_rom);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
