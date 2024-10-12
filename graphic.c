63bc765f4a63bb1a6865d556c03f52f8abd55f89
stardust <lql9705@gmail.com>
2024-10-12 13:18:28 +0900

    comment

--- graphic.c
+++ graphic.c
@@ -354,7 +354,7 @@ gbc_graphic_cycle(gbc_graphic_t *graphic)
           I thus suspect that the LY should be zero if the LCD is disabled. But I didn't
           find it in pandoc and gbdev. I googled "gameboy color ly lcdc disabled" and the
           first result is:
-          /* https://www.reddit.com/r/Gameboy/comments/a1c8h0/what_happens_when_a_gameboy_screen_is_disabled/
+          https://www.reddit.com/r/Gameboy/comments/a1c8h0/what_happens_when_a_gameboy_screen_is_disabled/
           which saved my day.
           I am very tired when writing this, but with a sense of accomplishment.
         */
